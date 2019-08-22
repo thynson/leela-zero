d765c86f7e3e439f11fd6f43c8b458b258ec98eb
Xingcan LAN <thynson@live.com>
2019-08-22 18:00:30 +0800

    fix game_started test

--- src/GameState.h
+++ src/GameState.h
@@ -65,7 +65,7 @@ public:
     void play_move(int color, int vertex);
     void play_move(int vertex);
     bool play_textmove(std::string color, const std::string& vertex);
-    bool game_started() { return !game_history.empty(); }
+    bool game_started() { return game_history.size() > 1; }
 
     void start_clock(int color);
     void stop_clock(int color);
